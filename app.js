852eadf4d61c94a22ec18cc5bfdb7947a2ce21d5
Omar Arredondo <cheuk102@gmail.com>
2022-03-22 11:06:19 -0600

    Server

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Omar Arredondo',
+    titulo: 'Curso Node'
+  });
+});
+
+app.get('/generic', function (req, res) {
+  res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', function (req, res) {
+  res.sendFile(__dirname + '/public/elements.html');
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
